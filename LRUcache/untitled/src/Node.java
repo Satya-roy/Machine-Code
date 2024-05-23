6ab58950a5b125b68fcd245025dcae98a13ced67
Satya <satya@192.168.1.2>
2024-05-23 13:48:50 +0530

    LRUCache

---
+++ LRUcache/untitled/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Node prev;
+    Node next;
+    int key;
+    int value;
+
+    Node(int _key, int _value) {
+        this.key = _key;
+        this.value = _value;
+    }
+}
